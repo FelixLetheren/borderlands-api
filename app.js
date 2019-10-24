bc58914ef18efc03b62e2d40a01e9a6d907f7a28
FelixLetheren <felix.letheren@gmail.com>
2019-10-24 15:35:16 +0100

    Changed get route to have promises sytax and send propper response JSONs.

--- app.js
+++ app.js
@@ -17,20 +17,39 @@ const dbName = 'borderlands'
 var getDataFromDb = (db, callback) => {
     var collection = db.collection('player-builds')
     collection.find({title: {$exists: true}}).toArray(function (err, docs) {
-        console.log("Found the following records:")
         callback(docs)
     })
 }
 
 app.get('/builds', (req, res) => {
-    MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
-        console.log('Connected to database')
-
-        let db = client.db(dbName)
-        getDataFromDb(db, (result) => {
-            console.log(result)
-            res.json(result)
+    MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then( client => {
+            let db = client.db(dbName)
+            let collection = db.collection('player-builds')
+            collection.find({}).toArray()
+                .then((docs) => {
+                    if (docs.length === 0) {
+                        return composeResponseJson(res, 500, false, 'Data not found', docs)
+                    }
+                    return composeResponseJson(res, 200, true, 'success', docs)
+                })
+                .catch (err => {
+                    console.log(err)
+                    return composeResponseJson(res, 500, false, 'Error getting data from database.', [])
+                })
+                .finally( () => {
+                    client.close()
+                })
+        }).catch( err => {
+            console.log(err)
+            return composeResponseJson(res, 500, false, 'Server error.', [])
         })
-    })
 })
 
+const composeResponseJson = function (res, status, success, message, data) {
+    return res.status(status).json({
+        success: success,
+        message: message,
+        data: data
+    })
+}
